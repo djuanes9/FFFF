887d33080c6e6504c1fddfef544bb83464d3f46e
Juan Armas <168566504+djuanes9@users.noreply.github.com>
2024-09-28 01:22:28 -0500

    Update InterfazHMI.jsx

--- src/components/InterfazHMI.jsx
+++ src/components/InterfazHMI.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { MQTTContext } from "./MQTTCliente"; // Importar el contexto global de MQTT
+import '../styles/styles.css'; // Estilos globales
 
 const HMI = () => {
   const { statuses } = useContext(MQTTContext); // Extraer los estados de los tópicos desde el contexto
