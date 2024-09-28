1143af766ccf30bf5cb97d1083638896d0572e40
Juan Armas <168566504+djuanes9@users.noreply.github.com>
2024-09-28 01:09:51 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App.jsx";
 import MQTTClient from "./components/MQTTCliente.jsx";
-import "./../public/styles.css";
+import "/styles.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
